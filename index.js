8eea48c97f3104b4ddad0a20904f8e5885a8036f
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-04-06 16:17:03 +0200

    Zadanie_13_7

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var colors = require('colors');
 fs.readdir('./','utf-8', function(err, files) {
     if (err) throw err;
     console.log('Before command'.red);
-    fs.writeFile('./text.txt', function(err) {
+    fs.writeFile('./text.txt', files, function(err) {
     	if (err) throw err;
     	console.log('Done!'.red);
-    	fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    	fs.readFile('./text.txt', 'utf-8', function(err, data) {
             if (err) throw err;
     		console.log('The file is composed of: '.red + data);
     	});
